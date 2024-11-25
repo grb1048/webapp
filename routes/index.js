29653423e5dad8968a48741fddd0cf4ab81f4782
Your Name <you@example.com>
2024-11-25 15:54:52 -0500

    hiiiiiiiiiiiii

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.send(`
+    <doctype HTML!>
+      <body>
+      <form action="/result">
+
+      <input type="number" name="numberInput"/>
+
+      <input type="submit" value="Submit!"/>
+      </form>
+      </body>`);
+})
+
+router.get('/result', function(req, res, next) {
+  let multnum = req.query.numberInput * 2;
+  res.send(`
+    <doctype HTML!>
+      <body>
+      <p>${multnum}</p>
+      </body>`);
+})
 
 module.exports = router;
